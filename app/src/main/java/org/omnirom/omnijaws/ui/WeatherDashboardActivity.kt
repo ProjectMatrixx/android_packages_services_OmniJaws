Lunaris-AOSP/packages_services_OmniJaws

adding omnijaws front-end
Change-Id: I0003aec7af1da7d0dc8b0c1a55fa5669ff4087aa
Signed-off-by: GHOST | ゴースト <90827302+Ghosuto@users.noreply.github.com>

app/src/main/java/org/omnirom/omnijaws/ui/WeatherDashboardActivity.kt
@@ -1,0 +1,76 @@
+/*
+ * Copyright 2025 AxionOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.omnirom.omnijaws.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.android.internal.util.android.OmniJawsClient
+import com.android.axion.compose.theme.AxionTheme
+
+class WeatherDashboardActivity : ComponentActivity(), OmniJawsClient.OmniJawsObserver {
+
+    private val viewModel: WeatherViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AxionTheme {
+                val uiState by viewModel.uiState.collectAsState()
+                WeatherDashboardScreen(
+                    uiState = uiState,
+                    onRefresh = { viewModel.forceRefresh() },
+                    onSettingsClick = { openSettings() },
+                    onLocationClick = { openLocationPicker() },
+                    getConditionIcon = { viewModel.getConditionIcon(it) }
+                )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        OmniJawsClient.get().addObserver(this, this)
+        viewModel.queryWeather()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        OmniJawsClient.get().removeObserver(this, this)
+    }
+
+    override fun weatherUpdated() {
+        viewModel.queryWeather()
+    }
+
+    override fun weatherError(errorReason: Int) {
+        viewModel.onWeatherError(errorReason)
+    }
+
+    private fun openSettings() {
+        startActivity(Intent(this, WeatherSettingsActivity::class.java))
+    }
+
+    private fun openLocationPicker() {
+        startActivity(Intent(this, LocationPickerActivity::class.java))
+    }
+}

app/src/main/java/org/omnirom/omnijaws/ui/WeatherSettingsActivity.kt
@@ -1,0 +1,122 @@
+/*
+ * Copyright 2025 AxionOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.omnirom.omnijaws.ui
+
+import android.Manifest
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.android.internal.util.android.OmniJawsClient
+import com.android.axion.compose.theme.AxionTheme
+
+class WeatherSettingsActivity : ComponentActivity(), OmniJawsClient.OmniJawsObserver {
+
+    private val viewModel: SettingsViewModel by viewModels()
+
+    private val locationPickerLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data = result.data ?: return@registerForActivityResult
+            val name = data.getStringExtra(LocationPickerActivity.DATA_LOCATION_NAME) ?: return@registerForActivityResult
+            val lat = data.getDoubleExtra(LocationPickerActivity.DATA_LOCATION_LAT, 0.0)
+            val lon = data.getDoubleExtra(LocationPickerActivity.DATA_LOCATION_LON, 0.0)
+            viewModel.setLocationResult(name, lat, lon)
+        }
+    }
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val granted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+        viewModel.onPermissionResult(granted)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AxionTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.surfaceContainer
+                ) {
+                val state by viewModel.uiState.collectAsState()
+                WeatherSettingsScreen(
+                    state = state,
+                    onBack = { finish() },
+                    onEnableChanged = { viewModel.setEnabled(it) },
+                    onProviderChanged = { viewModel.setProvider(it) },
+                    onUnitsChanged = { viewModel.setUnits(it) },
+                    onIntervalChanged = { viewModel.setUpdateInterval(it) },
+                    onCustomLocationChanged = {
+                        viewModel.setCustomLocation(it)
+                        if (!it) requestLocationPermissionIfNeeded()
+                    },
+                    onLocationPickerClick = {
+                        locationPickerLauncher.launch(
+                            Intent(this, LocationPickerActivity::class.java)
+                        )
+                    },
+                    onIconPackChanged = { viewModel.setIconPack(it) },
+                    onOwmKeyChanged = { viewModel.setOwmKey(it) },
+                    onRequestLocationPermission = { requestLocationPermissionIfNeeded() }
+                )
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        OmniJawsClient.get().addObserver(this, this)
+        viewModel.loadSettings()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        OmniJawsClient.get().removeObserver(this, this)
+    }
+
+    override fun weatherUpdated() {
+        viewModel.refreshUpdateStatus()
+    }
+
+    override fun weatherError(errorReason: Int) {}
+
+    private fun requestLocationPermissionIfNeeded() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != android.content.pm.PackageManager.PERMISSION_GRANTED) {
+            permissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+}

app/src/main/java/org/omnirom/omnijaws/ui/WeatherViewModel.kt
@@ -1,0 +1,87 @@
+/*
+ * Copyright 2025 AxionOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.omnirom.omnijaws.ui
+
+import android.app.Application
+import android.content.ContentValues
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.android.internal.util.android.OmniJawsClient
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class WeatherUiState(
+    val weatherInfo: OmniJawsClient.WeatherInfo? = null,
+    val isLoading: Boolean = true,
+    val error: Int? = null
+)
+
+class WeatherViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _uiState = MutableStateFlow(WeatherUiState())
+    private companion object {
+        const val TAG = "WeatherDashboard"
+    }
+    val uiState: StateFlow<WeatherUiState> = _uiState.asStateFlow()
+
+    private val client = OmniJawsClient.get()
+
+    fun queryWeather() {
+        val context = getApplication<Application>()
+        client.queryWeather(context)
+        val info = client.getWeatherInfo()
+        if (info != null) {
+            Log.d(TAG, "Weather data: city=${info.city} temp=${info.temp} condition=${info.condition}")
+            Log.d(TAG, "  humidity=${info.humidity} windSpeed=${info.windSpeed} pinWheel=${info.pinWheel}")
+            Log.d(TAG, "  feelsLike=${info.feelsLike} isNaN=${info.feelsLike.isNaN()}")
+            Log.d(TAG, "  pressure=${info.pressure} isNaN=${info.pressure.isNaN()}")
+            Log.d(TAG, "  uvi=${info.uvi} isNaN=${info.uvi.isNaN()}")
+            Log.d(TAG, "  visibility=${info.visibility} isNaN=${info.visibility.isNaN()}")
+            Log.d(TAG, "  dewPoint=${info.dewPoint} isNaN=${info.dewPoint.isNaN()}")
+            Log.d(TAG, "  sunrise=${info.sunrise} sunset=${info.sunset}")
+            Log.d(TAG, "  forecasts=${info.forecasts?.size} hourly=${info.hourlyForecasts?.size}")
+        } else {
+            Log.d(TAG, "Weather data: null")
+        }
+        _uiState.value = WeatherUiState(
+            weatherInfo = info,
+            isLoading = false,
+            error = if (info == null) OmniJawsClient.EXTRA_ERROR_DISABLED else null
+        )
+    }
+
+    fun onWeatherError(errorReason: Int) {
+        _uiState.value = _uiState.value.copy(error = errorReason, isLoading = false)
+    }
+
+    fun forceRefresh() {
+        _uiState.value = _uiState.value.copy(isLoading = true)
+        val context = getApplication<Application>()
+        val values = ContentValues()
+        values.put("update", true)
+        context.contentResolver.update(
+            Uri.parse("content://org.omnirom.omnijaws.provider/control"),
+            values, null, null
+        )
+    }
+
+    fun getConditionIcon(conditionCode: Int): Drawable? {
+        return client.getWeatherConditionImage(getApplication(), conditionCode)
+    }
+}

ProjectMatrixx/android_packages_services_OmniJaws

Signed-off-by: Mrick343 <mrinmoy8429de@gmail.com>

app/src/main/java/org/omnirom/omnijaws/ui/WeatherDashboardActivity.kt
@@ -1,0 +1,76 @@
+/*
+ * Copyright 2025 AxionOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.omnirom.omnijaws.ui
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.android.internal.util.matrixx.OmniJawsClient
+import com.android.axion.compose.theme.AxionTheme
+
+class WeatherDashboardActivity : ComponentActivity(), OmniJawsClient.OmniJawsObserver {
+
+    private val viewModel: WeatherViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AxionTheme {
+                val uiState by viewModel.uiState.collectAsState()
+                WeatherDashboardScreen(
+                    uiState = uiState,
+                    onRefresh = { viewModel.forceRefresh() },
+                    onSettingsClick = { openSettings() },
+                    onLocationClick = { openLocationPicker() },
+                    getConditionIcon = { viewModel.getConditionIcon(it) }
+                )
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        OmniJawsClient.get().addObserver(this, this)
+        viewModel.queryWeather()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        OmniJawsClient.get().removeObserver(this, this)
+    }
+
+    override fun weatherUpdated() {
+        viewModel.queryWeather()
+    }
+
+    override fun weatherError(errorReason: Int) {
+        viewModel.onWeatherError(errorReason)
+    }
+
+    private fun openSettings() {
+        startActivity(Intent(this, WeatherSettingsActivity::class.java))
+    }
+
+    private fun openLocationPicker() {
+        startActivity(Intent(this, LocationPickerActivity::class.java))
+    }
+}

app/src/main/java/org/omnirom/omnijaws/ui/WeatherSettingsActivity.kt
@@ -1,0 +1,122 @@
+/*
+ * Copyright 2025 AxionOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.omnirom.omnijaws.ui
+
+import android.Manifest
+import android.content.Intent
+import android.os.Bundle
+import android.provider.Settings
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import com.android.internal.util.matrixx.OmniJawsClient
+import com.android.axion.compose.theme.AxionTheme
+
+class WeatherSettingsActivity : ComponentActivity(), OmniJawsClient.OmniJawsObserver {
+
+    private val viewModel: SettingsViewModel by viewModels()
+
+    private val locationPickerLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val data = result.data ?: return@registerForActivityResult
+            val name = data.getStringExtra(LocationPickerActivity.DATA_LOCATION_NAME) ?: return@registerForActivityResult
+            val lat = data.getDoubleExtra(LocationPickerActivity.DATA_LOCATION_LAT, 0.0)
+            val lon = data.getDoubleExtra(LocationPickerActivity.DATA_LOCATION_LON, 0.0)
+            viewModel.setLocationResult(name, lat, lon)
+        }
+    }
+
+    private val permissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestMultiplePermissions()
+    ) { permissions ->
+        val granted = permissions[Manifest.permission.ACCESS_FINE_LOCATION] == true ||
+                permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+        viewModel.onPermissionResult(granted)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            AxionTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.surfaceContainer
+                ) {
+                val state by viewModel.uiState.collectAsState()
+                WeatherSettingsScreen(
+                    state = state,
+                    onBack = { finish() },
+                    onEnableChanged = { viewModel.setEnabled(it) },
+                    onProviderChanged = { viewModel.setProvider(it) },
+                    onUnitsChanged = { viewModel.setUnits(it) },
+                    onIntervalChanged = { viewModel.setUpdateInterval(it) },
+                    onCustomLocationChanged = {
+                        viewModel.setCustomLocation(it)
+                        if (!it) requestLocationPermissionIfNeeded()
+                    },
+                    onLocationPickerClick = {
+                        locationPickerLauncher.launch(
+                            Intent(this, LocationPickerActivity::class.java)
+                        )
+                    },
+                    onIconPackChanged = { viewModel.setIconPack(it) },
+                    onOwmKeyChanged = { viewModel.setOwmKey(it) },
+                    onRequestLocationPermission = { requestLocationPermissionIfNeeded() }
+                )
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        OmniJawsClient.get().addObserver(this, this)
+        viewModel.loadSettings()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        OmniJawsClient.get().removeObserver(this, this)
+    }
+
+    override fun weatherUpdated() {
+        viewModel.refreshUpdateStatus()
+    }
+
+    override fun weatherError(errorReason: Int) {}
+
+    private fun requestLocationPermissionIfNeeded() {
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != android.content.pm.PackageManager.PERMISSION_GRANTED) {
+            permissionLauncher.launch(
+                arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                )
+            )
+        }
+    }
+}

app/src/main/java/org/omnirom/omnijaws/ui/WeatherViewModel.kt
@@ -1,0 +1,87 @@
+/*
+ * Copyright 2025 AxionOS Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.omnirom.omnijaws.ui
+
+import android.app.Application
+import android.content.ContentValues
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
+import com.android.internal.util.matrixx.OmniJawsClient
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+data class WeatherUiState(
+    val weatherInfo: OmniJawsClient.WeatherInfo? = null,
+    val isLoading: Boolean = true,
+    val error: Int? = null
+)
+
+class WeatherViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val _uiState = MutableStateFlow(WeatherUiState())
+    private companion object {
+        const val TAG = "WeatherDashboard"
+    }
+    val uiState: StateFlow<WeatherUiState> = _uiState.asStateFlow()
+
+    private val client = OmniJawsClient.get()
+
+    fun queryWeather() {
+        val context = getApplication<Application>()
+        client.queryWeather(context)
+        val info = client.getWeatherInfo()
+        if (info != null) {
+            Log.d(TAG, "Weather data: city=${info.city} temp=${info.temp} condition=${info.condition}")
+            Log.d(TAG, "  humidity=${info.humidity} windSpeed=${info.windSpeed} pinWheel=${info.pinWheel}")
+            Log.d(TAG, "  feelsLike=${info.feelsLike} isNaN=${info.feelsLike.isNaN()}")
+            Log.d(TAG, "  pressure=${info.pressure} isNaN=${info.pressure.isNaN()}")
+            Log.d(TAG, "  uvi=${info.uvi} isNaN=${info.uvi.isNaN()}")
+            Log.d(TAG, "  visibility=${info.visibility} isNaN=${info.visibility.isNaN()}")
+            Log.d(TAG, "  dewPoint=${info.dewPoint} isNaN=${info.dewPoint.isNaN()}")
+            Log.d(TAG, "  sunrise=${info.sunrise} sunset=${info.sunset}")
+            Log.d(TAG, "  forecasts=${info.forecasts?.size} hourly=${info.hourlyForecasts?.size}")
+        } else {
+            Log.d(TAG, "Weather data: null")
+        }
+        _uiState.value = WeatherUiState(
+            weatherInfo = info,
+            isLoading = false,
+            error = if (info == null) OmniJawsClient.EXTRA_ERROR_DISABLED else null
+        )
+    }
+
+    fun onWeatherError(errorReason: Int) {
+        _uiState.value = _uiState.value.copy(error = errorReason, isLoading = false)
+    }
+
+    fun forceRefresh() {
+        _uiState.value = _uiState.value.copy(isLoading = true)
+        val context = getApplication<Application>()
+        val values = ContentValues()
+        values.put("update", true)
+        context.contentResolver.update(
+            Uri.parse("content://org.omnirom.omnijaws.provider/control"),
+            values, null, null
+        )
+    }
+
+    fun getConditionIcon(conditionCode: Int): Drawable? {
+        return client.getWeatherConditionImage(getApplication(), conditionCode)
+    }
+}